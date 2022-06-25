b70b408b6aefc8120a063b6a057abdd734789e64
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2022-06-25 15:23:42 -0300

    Post Entrega

--- SigmaDelta/SigmaDelta.py
+++ SigmaDelta/SigmaDelta.py
@@ -1,36 +1,8 @@
-# http://blog.espol.edu.ec/estg1003/sigma-delta-codificador-con-python/
-
-from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 import scipy.signal as ss
 import scipy.fft as fft
-
-def graph(t, muestra, xi, ydig, ydecim, q):
-    # n0=0; deltaN=int(len(t)/1000); nf=n0+deltaN
-    # plt.figure(1)
-    # plt.suptitle('Conversor $\Sigma \Delta$')
-
-    # # Comp muestra con xi
-    # plt.subplot(211)    
-    # plt.ylabel('x(t), x[n]')
-    # plt.plot(t[n0:nf],muestra[n0:nf], 'g')
-    # plt.step(t[n0:nf],xi[n0:nf], where='post',color='m')
-
-    # # Grafica de y[n]
-    # plt.subplot(212)
-    # plt.ylabel('y[n]')
-    # plt.axis((n0,nf,-0.1,1.1))
-    # puntos=np.arange(n0,nf,1)
-    # puntosdec=np.arange(n0,nf,q); puntosdec=np.append(puntosdec,nf)
-    # plt.step(puntos, ydig[n0:nf], where='post', label='y[n]', color='b')
-    # plt.step(puntosdec[n0:nf], ydecim[n0:int(nf/q)+2], where='post', label='decimado', color='r')
-    # plt.legend()
-    # plt.tight_layout()
-    # plt.show()
-
-    return 0
 
 def FAA(fb, muestra, t, verbose=False):
     b, a = ss.butter(N=1, Wn=np.pi*fb, btype='lowpass', analog=True)
@@ -43,15 +15,15 @@ def FAA(fb, muestra, t, verbose=False):
 
     return yout
 
-def conversor(k, filtered, deltaY, t, noise=False, fs=44100):
-    yanalog=np.zeros(k, dtype=float)
-    ydig=np.zeros(k, dtype=int)
-    xi=np.zeros(k, dtype=float)
-    diferencia=np.zeros(k, dtype=float)
+def conversor(k, filtered, deltaY):
+    yanalog=np.zeros(k)
+    ydig=np.zeros(k)
+    xi=np.zeros(k)
+    diferencia=np.zeros(k)
 
     for i in range(0,k):
         diferencia[i] = filtered[i]-yanalog[i-1] if i>0 else filtered[i]    #Sumador
-        xi[i] = xi[i-1]+diferencia[i] if i>0 else diferencia[i]             #Integrador                                               #Ruido   
+        xi[i] = xi[i-1]+diferencia[i] if i>0 else diferencia[i]             #Integrador                                                 
         ydig[i] = 1 if xi[i]>0 else 0                                       #Cuantizador
         yanalog[i]= deltaY if ydig[i]==1 else -deltaY                       #Realimentación
     return ydig, xi, diferencia
@@ -87,7 +59,7 @@ def noiseShapping(k):
         yanalog[i]= ydig[i]                                                 #Realimentación
     return ydig, ruido   
 
-fb=22000; fs=44100; q=4; fos=fs*q
+fb=22000; fs=44100; q=2**4; fos=fs*q
 
 archivo = 'SigmaDelta\muestra.wav'
 fdata, data = wav.read(archivo)
@@ -101,7 +73,7 @@ filtered = FAA(fb, data/np.max(data), t, verbose=False)
 k=len(filtered)
 
 #Conversor
-ydig, xi, diff= conversor(k, filtered, deltaY, t, noise=False, fs=fs)
+ydig, xi, diff= conversor(k, filtered, deltaY)
 
 #DownSampler
 ysalida=np.zeros(int(k/q))
@@ -112,7 +84,7 @@ puntos=(np.arange(n0+1,nf+1,1))/fos
 
 #Grafico entrada-salida
 plt.plot(t[n0:nf],filtered[n0:nf], 'g', label= "x(t)")
-plt.step(puntos, (ydig[n0:nf]-0.5)*2, where='mid', color='k', alpha=0.2, label= "y[n]")
+plt.step(puntos, (ydig[n0:nf]-0.5)*1.67, where='mid', color='k', alpha=0.2, label= "y[n]")
 plt.legend(loc='lower right')
 plt.show()
 
@@ -128,20 +100,25 @@ plt.show()
 puntos=(np.arange(n0,nf,1))/fos
 plt.plot(t[n0:nf],filtered[n0:nf], 'green', label= "x(t)")
 plt.step(puntos, (ydig[n0:nf]-0.5)*1.7, where='mid', color='k', alpha=0.2, label= "y[n]")
-plt.step(puntos*fos/fs, (ysalida[n0:nf]-0.5)*1.7, where='mid', color='blue', alpha=0.5, label= "Decimada")
-plt.xlim(0,0.001)
+plt.step((puntos*fos/fs)[0:int(nf/q)], (ysalida[n0:int(nf/q)]-0.5)*1.7, where='mid', color='blue', alpha=0.5, label= "Decimada")
+
 plt.legend(loc='upper right')
 plt.show()
 
-# FFT
+# FFT del ruido a la salida
 noisydata, ruido= noiseShapping(k)                              # Señal con ruido
 noiseout_fft = 2.0 / k * np.abs(fft.fft(noisydata)[1:k // 2])   # Espectro salida
 noise_fft = 2.0 / k * np.abs(fft.fft(ruido)[1:k // 2])          # Espectro ruido
 freqs = fft.fftfreq(k, 1 / fos)[1:k // 2]
+#data_fft = 2.0 / k * np.abs(fft.fft(data)[1:k // 2])            # Espectro ruido
+#freqsdata = fft.fftfreq(data.size, 1 / fs)[1:k // 2]
 plt.plot(freqs, noiseout_fft, 'g', label= "Noise(f)")           # Plot espectro salida
+#plt.plot(freqsdata, data_fft, 'b', label= "data(f)")            # Plot espectro Entrada
+plt.ylim(0,0.02)
 plt.legend()
 plt.show()
 
+# Grafico de la NTF
 plt.title("NTF")
 plt.plot(freqs,noiseout_fft/noise_fft, label="NTF")             # Plot transferencia
 plt.legend()
